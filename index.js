d9ab3c3faeda8c02a50763cac0f964ccdc0f5bfd
Jakyp-05 <jakypjumagulov080gmail.com>
2023-11-07 10:40:37 +0600

    added new shop, index.html, style.css, index.js

--- index.js
+++ index.js
@@ -3,42 +3,65 @@ let products = [
         id: 1,
         image: "./assets/images/1.png",
         title: "Calabrese Broccoli",
-        span: "20.00"
+        span: "20.00",
+        price: "13.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 2,
         image: "./assets/images/2.png",
         title: "Fresh Banana Fruites",
+        span: "20.00",
+        price: "14.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 3,
         image: "./assets/svg/3.svg",
         title: "White Nuts",
+        span: "20.00",
+        price: "15.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 4,
         image: "./assets/svg/4.svg",
         title: "Vegan Red Tomato",
+        span: "20.00",
+        price: "17.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 5,
-        image: "./assets/svg/1.svg",
+        image: "./assets/svg/5.svg",
         title: "Mung Bean",
+        span: "20.00",
+        price: "11.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 6,
-        image: "./assets/svg/1.svg",
+        image: "./assets/svg/6.svg",
         title: "Brown Hazelnut",
+        span: "20.00",
+        price: "12.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 7,
-        image: "./assets/svg/1.svg",
+        image: "./assets/svg/7.svg",
         title: "Eggs",
+        span: "20.00",
+        price: "17.00",
+        img: "./assets/svg/rating.svg"
     },
     {
         id: 8,
-        image: "./assets/svg/1.svg",
+        image: "./assets/svg/8.svg",
         title: "Zelco Suji Elaichi Rusk",
+        span: "20.00",
+        price: "15.00",
+        img: "./assets/svg/rating.svg"
     },
 ];
 
@@ -47,16 +70,16 @@ const shopContentProducBox = document.querySelector(".shop-content-product-box")
 let showCart = products.map((item) => {
     return `
        <div class="box">
-          <img src="${item.image}" alt="alt"/>
+          <img width="350px" height="350px" src="${item.image}" alt="alt"/>
           <div class="rating">
               <h2>${item.title}</h2>
-              <div class="rating-div">
+            <div class="rating-div">
                 <div class="rating-div-price">
                    <span>${item.span}</span>
-                   <p></p>
+                   <p>${item.price} $</p>
                 </div>
-                <img src="" alt=""/>
-              </div>
+                <img src="${item.img}" alt="alt-img"/>
+            </div>
           </div>
        </div>
     `
